2c199d73b5a8412b26ceae189e935c62a7dfe959
Ruben Vivas <ext.rvivas@servipag.cl>
2020-11-03 14:56:29 -0300

    add increase balance

--- src/paths/routes.js
+++ src/paths/routes.js
@@ -2,10 +2,8 @@ const express = require('express');
 
 const app = express();
 
-
 //app.use(require('./user'));
 app.use(require('./login'));
-
+app.use(require('./user'));
 
 module.exports = app;
-
--- src/paths/user.js
+++ src/paths/user.js
@@ -1,1 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const UserInfo = require('../models/user');
 
+const app = new express();
+app.put('/incraseBalance', cors(), async (req, res) => {
+  let { username, addToBalance } = req.body;
+
+  const user = await UserInfo.findOne({ username });
+  console.log(user);
+  user.balance += addToBalance;
+  const result = await UserInfo.findOneAndUpdate({ username: user.username }, user);
+  console.log(result);
+  return res.status(200).json({
+    ok: true,
+    message: 'Balance updated',
+  });
+});
+
+module.exports = app;
