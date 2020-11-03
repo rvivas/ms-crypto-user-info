dd8d6a86e27201e205ea1d36e1b9c178faf95953
Ruben Vivas <ext.rvivas@servipag.cl>
2020-11-02 23:55:50 -0300

    add cors

--- src/paths/login.js
+++ src/paths/login.js
@@ -1,50 +1,46 @@
 const express = require('express');
-const bcrypt = require('bcrypt')
-
+const bcrypt = require('bcrypt');
+const cors = require('cors');
 const UserInfo = require('../models/user');
 
 const app = express();
 
+app.get('/login', cors(), (req, res) => {
+  let { username, password } = req.body;
 
-app.get('/login', (req, res) => {
-     
-       let { username, password } = req.body
-       
-       UserInfo.findOne({username}, (err,userDB) => {
-	if(err){
-	    return res.status(500).json({
-		ok: false,
-		err
-	    })
-	} 
-        console.log(userDB)	
-	 if(!userDB){
-	   console.log('info no encontrada')
-		  return res.status(400).json({
-		      ok: false,
-		      err: {
-			message: 'Invalid user or credentials'
-		      }
-		  })
-	      }
-	 console.log('usuario encontrado')
-	 if(!bcrypt.compare(password, userDB.password )){
-	   console.log('password invalido')
+  UserInfo.findOne({ username }, (err, userDB) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        err,
+      });
+    }
+    console.log(userDB);
+    if (!userDB) {
+      console.log('info no encontrada');
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: 'Invalid user or credentials',
+        },
+      });
+    }
+    console.log('usuario encontrado');
+    if (!bcrypt.compare(password, userDB.password)) {
+      console.log('password invalido');
 
-	    return res.status(400).json({
-		ok:false,
-		err: {
-		  message: 'Invalid user or credentials'
-		}
-	    })
-	 }
-	 res.json({
-	    ok: true,
-	    usuario: userDB
-	 })
-       })
-         
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: 'Invalid user or credentials',
+        },
+      });
+    }
+    res.json({
+      ok: true,
+      usuario: userDB,
+    });
+  });
 });
 
-module.exports = app
-
+module.exports = app;
